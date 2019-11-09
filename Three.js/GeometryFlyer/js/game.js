f65ccf3b70e0e43181f962f25a10d503965b6f37
ColgateLeoAscenzi <lascenzi@colgate.edu>
2019-11-08 21:23:34 -0500

    added more model refactoring

--- Three.js/GeometryFlyer/js/game.js
+++ Three.js/GeometryFlyer/js/game.js
@@ -111,115 +111,7 @@ function createLights() {
 
 }
 
-
-
 var geomCockpit;
-var AirPlane = function(){
-
-  this.mesh      = new THREE.Object3D();
-  this.mesh.name = "airPlane";
-
-	  // Create the Cabin
-  var geomHead = new THREE.BoxGeometry(20, 20, 20, 1, 1, 1);
-  var matHead  = new THREE.MeshPhongMaterial(
-                             { color : Colors.pink,
-                             shading : THREE.FlatShading });
-
-  var head = new THREE.Mesh(geomHead, matHead);
-
-  head.castShadow = true;
-  head.receiveShadow = true;
-
-  head.position.y = 40;
-  this.mesh.add(head);
-
-  this.mesh.add(Pilot());
-
-  // Create the Cabin
-  geomCockpit = new THREE.BoxGeometry(60, 50, 50, 1, 1, 1);
-  var matCockpit  = new THREE.MeshPhongMaterial(
-                             { color : Colors.red,
-                             shading : THREE.FlatShading });
-  var cockpit = new THREE.Mesh(geomCockpit, matCockpit);
-
-  cockpit.castShadow = true;
-  cockpit.receiveShadow = true;
-
-  this.mesh.add(cockpit);
-
-
-  // Create Engine
-  var geomEngine = new THREE.BoxGeometry(20, 50, 50, 1, 1, 1);
-  var matEngine  = new THREE.MeshPhongMaterial(
-                            { color : Colors.white,
-                            shading : THREE.FlatShading });
-
-  var engine = new THREE.Mesh(geomEngine, matEngine);
-
-  engine.position.x = 40;
-  engine.castShadow = true;
-  engine.receiveShadow = true;
-
-  this.mesh.add(engine);
-
-
-   // Create Tailplane
-  var geomTailPlane = new THREE.BoxGeometry(15, 20, 5, 1, 1, 1);
-  var matTailPlane  = new THREE.MeshPhongMaterial(
-                               { color : Colors.red,
-                               shading : THREE.FlatShading });
-
-  var tailPlane = new THREE.Mesh(geomTailPlane, matTailPlane);
-
-  tailPlane.position.set(-35, 25, 0);
-  tailPlane.castShadow = true;
-  tailPlane.receiveShadow = true;
-
-  this.mesh.add(tailPlane);
-
-
-  // Create Wing
-  var geomSideWing = new THREE.BoxGeometry(40, 8, 150, 1, 1, 1);
-  var matSideWing  = new THREE.MeshPhongMaterial(
-                              { color : Colors.red,
-                              shading : THREE.FlatShading });
-
-  var sideWing = new THREE.Mesh(geomSideWing, matSideWing);
-  sideWing.position.set(0, 0, 0);
-  sideWing.castShadow = true;
-  sideWing.receiveShadow = true;
-
-  this.mesh.add(sideWing);
-
-
-  // Propeller
-  var geomPropeller = new THREE.BoxGeometry(20, 10, 10, 1, 1, 1);
-  var matPropeller  = new THREE.MeshPhongMaterial(
-                            { color : Colors.brown,
-                            shading : THREE.FlatShading });
-  this.propeller = new THREE.Mesh(geomPropeller, matPropeller);
-  this.propeller.castShadow = true;
-  this.propeller.receiveShadow = true;
-
-  // Blades
-  var geomBlade = new THREE.BoxGeometry(1, 100, 20, 1, 1, 1);
-  var matBlade = new THREE.MeshPhongMaterial(
-                           { color : Colors.brownDark,
-                           shading : THREE.FlatShading });
-
-  var blade = new THREE.Mesh(geomBlade, matBlade);
-  blade.position.set(8, 0, 0);
-  blade.castShadow = true;
-  blade.receiveShadow = true;
-
-  this.propeller.add(blade);
-  this.propeller.position.set(50, 0, 0);
-
-  this.mesh.add(this.propeller);
-
-};
-
-
 
 Sky = function() {
   this.mesh = new THREE.Object3D();
@@ -449,7 +341,20 @@ function updatePlaneView(){
     	camera.position.x = airplane.mesh.position.x-100;
     	camera.position.y = airplane.mesh.position.y+100;
     	camera.lookAt(new THREE.Vector3(airplane.mesh.position.x+1000,  airplane.mesh.position.y-500, airplane.mesh.position.z));
+
+//Aim down sights left
+      // camera.position.z = airplane.mesh.position.z-10;
+      // camera.position.x = airplane.mesh.position.x-10;
+      // camera.position.y = airplane.mesh.position.y+20;
+      // camera.lookAt(new THREE.Vector3(airplane.mesh.position.x+1000,  airplane.mesh.position.y-500, airplane.mesh.position.z-100));
+
+  //right isometric
+      // camera.position.z = airplane.mesh.position.z+40;
+      // camera.position.x = airplane.mesh.position.x-30;
+      // camera.position.y = airplane.mesh.position.y+30;
+      // camera.lookAt(new THREE.Vector3(airplane.mesh.position.x+1000,  airplane.mesh.position.y-500, airplane.mesh.position.z-500));
 	}
+
 	else{
 	  camera.position.x = 0;
       camera.position.z = 200;
